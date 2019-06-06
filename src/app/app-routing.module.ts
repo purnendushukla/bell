da17f9d85d7577f2d5613d1a8f4cade44225ac14
Purnendu Shukla <purnendushukla@hotmail.com>
2019-06-07 02:02:29 +0530

    basic forms added and 2 basic routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserLoginComponent} from "./user-login/user-login.component";
+import {UserConvoPortalComponent} from "./user-convo-portal/user-convo-portal.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' ,component:UserLoginComponent},
+  {path: 'login', component: UserLoginComponent},
+  {path: 'userConvoPortal', component: UserConvoPortalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
